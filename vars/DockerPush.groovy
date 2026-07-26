234db2c55d8480d3a5d8cf9f47b06d86efd91eb9
Jay Vaghela <welcomesir0304@gmail.com>
2026-07-26 20:49:57 +0530

    Fix syntax error in DockerPush.groovy

--- vars/DockerPush.groovy
+++ vars/DockerPush.groovy
@@ -1,4 +1,4 @@
-de call(String Project, String ImageTag, String dockerHubuser){
+def call(String Project, String ImageTag, String dockerHubuser){
     withCredentials([usernamePassword(credentialsId: 'dockerHubCred', passwordVariale: 'dockerhubPass', usernmeVariable: 'dockerHubUser')]){
         sh "docker login -u ${dockerHubUser} -p ${dockerhubPass}"
     }
