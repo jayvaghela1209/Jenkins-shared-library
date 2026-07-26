4490b3e283d9d11bbf3aa10524e632d4683f9e63
Jay Vaghela <welcomesir0304@gmail.com>
2026-07-26 20:09:18 +0530

    Update BuildCode.groovy to include Docker Hub user

--- vars/BuildCode.groovy
+++ vars/BuildCode.groovy
@@ -1,3 +1,3 @@
-def call(String project, String version){
-  sh "docker build -t "${project}:"${version}" . "
+def call(String project, String ImageTag, String dockerHubUser){
+  sh "docker build -t ${dockerHubUser}/${project}:${ImageTag} . "
 }
