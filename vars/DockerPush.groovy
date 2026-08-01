8b518e015c6617e11b8cd787c6997e6b4201073e
Jay Vaghela <welcomesir0304@gmail.com>
2026-08-02 01:38:13 +0530

    Remove Docker image tagging before push
    Remove the tagging step before pushing the Docker image.

--- vars/DockerPush.groovy
+++ vars/DockerPush.groovy
@@ -2,7 +2,6 @@ def call(String imageName, String tag, String dockerHubUser) {
       withCredentials([usernamePassword(credentialsId: 'DockerHubCred',
                         usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
           sh "echo \$DOCKER_PASS | docker login -u \$DOCKER_USER --password-stdin"
-          sh "docker image tag ${imageName}:${tag} ${dockerHubUser}/${imageName}:${tag}"
           sh "docker push ${dockerHubUser}/${imageName}:${tag}"
       }
   }
