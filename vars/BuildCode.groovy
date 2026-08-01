3562e325c1bfb52aaf9236c2a749a3b099b5d7ec
Jay Vaghela <welcomesir0304@gmail.com>
2026-08-02 01:58:28 +0530

    Add echo for dockerHubUser in BuildCode.groovy
    Added echo statement to display dockerHubUser.

--- vars/BuildCode.groovy
+++ vars/BuildCode.groovy
@@ -1,4 +1,5 @@
 def call(String project, String imageTag, String dockerHubUser) {
     sh "docker build -t ${dockerHubUser}/${project}:${imageTag} ."
     sh "docker images"
+    echo "dockerHubUser = ${env.dockerHubUser}"
 }
