148b5377eddcca3a99df3e565e751e147af139e2
Jay Vaghela <welcomesir0304@gmail.com>
2026-08-02 01:06:59 +0530

    Add Docker image tagging before pushing

--- vars/DockerPush.groovy
+++ vars/DockerPush.groovy
@@ -2,6 +2,7 @@ def call(String imageName, String tag, String dockerHubUser) {
       withCredentials([usernamePassword(credentialsId: 'DockerHubCred',
                         usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
           sh "echo \$DOCKER_PASS | docker login -u \$DOCKER_USER --password-stdin"
+          sh "docker image tag ${imageName}:${tag} ${dockerHubUser}/${imageName}:${tag}"
           sh "docker push ${dockerHubUser}/${imageName}:${tag}"
       }
   }
